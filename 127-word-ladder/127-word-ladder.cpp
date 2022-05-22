2c76721440a7ca9ad301fc1cf903c1188f9d93f1
anucse <87842739+anucse@users.noreply.github.com>
2022-05-22 14:14:55 +0530

    Time: 152 ms (66.09%), Space: 13.7 MB (92.87%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int ladderLength(string begin, string end, vector<string>& word) {
+        unordered_set<string> word_set(word.begin(),word.end());
+        int ans=1;
+        if(word_set.find(end)==word_set.end() )
+            return 0;
+        queue<string> q;
+        q.push(begin);
+        while(!q.empty()){
+            ans++;
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                string top=q.front();
+                q.pop();
+                if(top==end)
+                    return ans;
+                
+                for(int j=0;j<top.size();j++){
+                    string temp=top;
+                    for(char k='a';k<='z';k++){
+                        top[j]=k;
+                        if(temp==top)
+                            continue;
+                        if(top==end)
+                            return ans;
+                        if(word_set.find(top)!=word_set.end()){
+                            q.push(top);
+                            word_set.erase(top);
+                        }
+                    }
+                    top=temp;
+                }
+            }
+            
+        }
+        return 0;
+    }
+};
