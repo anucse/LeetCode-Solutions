a1ffc9b717c9e2d6aa15ba724ebc151560f74b72
anucse <87842739+anucse@users.noreply.github.com>
2022-05-24 08:11:35 +0530

    Time: 142 ms (35.80%), Space: 42.1 MB (13.31%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        int degree_sum,ans=0;
         vector<unordered_set<int>> graph(n);
+        int ans=0;
         for(vector<int> x:roads){
             graph[x[0]].insert(x[1]);
             graph[x[1]].insert(x[0]);
@@ -10,13 +10,14 @@ public:
         
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                degree_sum=graph[i].size()+graph[j].size();
-                if(graph[i].find(j)!=graph[i].end())
-                    degree_sum-=1;
-                ans=max(ans,degree_sum);
+                int degree=graph[i].size()+graph[j].size();
+                if(graph[i].find(j)!=graph[i].end()){
+                    degree--;
+                    
+                }
+                ans=max(ans,degree);
             }
         }
-        
         return ans;
     }
 };
