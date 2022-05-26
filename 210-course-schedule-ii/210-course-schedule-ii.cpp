b0d7a860a66a76ec015c5ef8b7e98a72cf38efe3
anucse <87842739+anucse@users.noreply.github.com>
2022-05-26 09:13:08 +0530

    Time: 70 ms (6.07%), Space: 14.7 MB (30.17%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -5,30 +5,31 @@ public:
     vector<int> indegree;
     
     void dfs(int node){
-        ans.push_back(node);
+        
         indegree[node]=-1;
-        for(int x:graph[node]){
+        for(int x: graph[node]){
             indegree[x]-=1;
             if(indegree[x]==0){
-                
+                ans.push_back(x);
                 dfs(x);
             }
-                
         }
     }
-    
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        graph = vector<vector<int>> (n);
-        indegree=vector<int> (n,0);
+        graph.resize(n);
+        indegree.resize(n,0);
         
         for(vector<int> x:pre){
-            indegree[x[0]]++;
             graph[x[1]].push_back(x[0]);
+            indegree[x[0]]++;
         }
         
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0)
+        for(int i=0;i<n;++i){
+            if(indegree[i]==0){
+                ans.push_back(i);
                 dfs(i);
+            }
+                
         }
         
         if(ans.size()==n)
