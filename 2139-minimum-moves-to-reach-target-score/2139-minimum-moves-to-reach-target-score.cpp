a15eb4851481b24ce1e97a747fc7e1d2a2806f41
anucse <87842739+anucse@users.noreply.github.com>
2022-05-27 08:29:19 +0530

    Time: 5 ms (15.53%), Space: 6 MB (25.93%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int minMoves(int target, int maxDoubles) {
         int ans=0;
-        while(target>1){
-            if(target%2==0 && maxDoubles){
+        while(target>1 && maxDoubles){
+            if(target%2==0){
                 target=target>>1;
                 maxDoubles-=1;
             }
@@ -11,9 +11,9 @@ public:
                 target-=1;
             }
             ans++;
-            if(maxDoubles==0)
-                return (target-1)+ans;
         }
+        if(!maxDoubles)
+            return (target-1+ans);
         return ans;
     }
 };
