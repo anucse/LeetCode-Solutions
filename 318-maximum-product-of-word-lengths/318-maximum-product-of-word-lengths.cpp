d04a24f740af94a20390ba3df1b5db13e97e6d29
anucse <87842739+anucse@users.noreply.github.com>
2022-05-29 12:28:23 +0530

    Time: 40 ms (96.02%), Space: 15.7 MB (64.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    
-    bool common(bitset<26> &w1,bitset<26> &w2){
-        for(int i=0;i<26;i++){
-            if(w1[i] && w2[i])
-                return true;
-        }
-        return false;
-    }
-    
     int maxProduct(vector<string>& words) {
         int ans=0;
         int n=words.size();
-        vector<bitset<26>> chars(n);
+        vector<int> mask(n);
         for(int i=0;i<n;i++){
             for(char x:words[i]){
-                chars[i][x-'a']=1;
+                mask[i]=mask[i] | 1<<(x-'a');
             }
             for(int j=0;j<i;j++){
-                if(!common(chars[i],chars[j])){
-                    int len = words[i].size() * words[j].size();
+                if(!(mask[i] & mask[j])){
+                    int len=words[i].size() * words[j].size();
                     ans=max(ans,len);
                 }
             }
