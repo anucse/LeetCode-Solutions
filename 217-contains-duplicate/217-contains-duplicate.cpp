8ef54946a7230ef76b44669c1ad4fcbc0403a61b
anucse <87842739+anucse@users.noreply.github.com>
2022-06-08 19:06:54 +0530

    Time: 161 ms (23.39%), Space: 51.4 MB (50.95%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int x:nums){
+            m[x]++;
+            if(m[x]==2)
+                return true;
+        }
+        return false;
+    }
+};
