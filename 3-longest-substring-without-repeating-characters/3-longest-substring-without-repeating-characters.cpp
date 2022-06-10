c9197257daf3887d432c9744049e2ef0a5d43a98
anucse <87842739+anucse@users.noreply.github.com>
2022-06-10 08:04:28 +0530

    Time: 49 ms (24.41%), Space: 10.7 MB (30.93%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,11 +3,10 @@ public:
     int lengthOfLongestSubstring(string s) {
         int l=0;
         int r=0;
+        int n=s.size();
         int len=0;
         
         unordered_set<int> st;
-        int n=s.size();
-        
         while(r<n){
             if(st.find(s[r])==st.end()){
                 st.insert(s[r]);
