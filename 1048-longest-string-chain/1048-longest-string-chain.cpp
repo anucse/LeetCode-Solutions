2d843485b2b9d101a30b7bc69b660b621c5d7523
anucse <87842739+anucse@users.noreply.github.com>
2022-06-15 09:58:53 +0530

    Time: 129 ms (57.55%), Space: 13.1 MB (94.23%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    bool compare(string &s1,string &s2){
+    static bool increasing_len(string &s1, string &s2){
+        return s1.size()<s2.size();
+    }
+    
+    bool isPredecessor(string &s1, string &s2){
         int n1=s1.size();
         int n2=s2.size();
-        
-        if(n1!=n2-1)
+        if(n1 !=n2-1){
             return false;
-        int i =0, j=0;
+        }
+        
+        int i=0;
+        int j=0;
         while(j<n2){
             if(s1[i]==s2[j]){
                 i++;
                 j++;
             }
-            else
+            else{
                 j++;
+            }
         }
+        
         if(i==n1 && j==n2)
             return true;
         return false;
-        
-    }
-    static bool increasing_len(string &s1,string &s2){
-        return s1.size()<s2.size();
     }
     int longestStrChain(vector<string>& words) {
         sort(words.begin(),words.end(),increasing_len);
-        vector<int> dp(words.size(),1);
+        int n=words.size();
         int max_len=1;
-        for(int i=0;i<words.size();i++){
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                if(compare(words[j],words[i]) and dp[i]<dp[j]+1){
+                if(isPredecessor(words[j],words[i]) && dp[i]<dp[j]+1){
                     dp[i]=dp[j]+1;
                 }
             }
