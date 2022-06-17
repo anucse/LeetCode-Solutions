a3e2a87b08e8b3d81736332ad59954af2edbeceb
anucse <87842739+anucse@users.noreply.github.com>
2022-06-17 11:09:16 +0530

    Time: 240 ms (53.90%), Space: 61.6 MB (33.22%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.cpp
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -1,41 +1,57 @@
 class Solution {
 public:
+    // Topological sorting 
+    // start from minimum indegree nodes , they are leaf nodes 
+    // remove leaves 
+    // at last , remaining one or two nodes are the root of min height tree 
+    
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n==1)
-            return {0};
         
         vector<int> ans;
+        if(n==1)
+            return {0};
+        // create an indegree vector 
+        vector<int> indegree(n,0);
+        for(vector x:edges){
+            indegree[x[0]]++;
+            indegree[x[1]]++;
+        }
         
+        // create an adjacency graph 
         vector<vector<int>> graph(n);
-        vector<int> degree(n,0);
-        
         for(vector<int> x:edges){
             graph[x[0]].push_back(x[1]);
             graph[x[1]].push_back(x[0]);
-            degree[x[0]]++;
-            degree[x[1]]++;
         }
         
+        // BFS approach , create a queue 
         queue<int> q;
+        
+        //push all the leaf nodes(nodes with indegree 1) into the queue
         for(int i=0;i<n;i++){
-            if(degree[i]==1)
+            if (indegree[i]==1)
                 q.push(i);
         }
         
+        int s;
         while(!q.empty()){
-            int s=q.size();
             ans.clear();
+            s=q.size();
+    
             for(int i=0;i<s;i++){
                 int top=q.front();
                 q.pop();
                 ans.push_back(top);
                 for(int x:graph[top]){
-                    degree[x]-=1;
-                    if(degree[x]==1)
+                    indegree[x]--;
+                    if(indegree[x]==1) // if becomes leaf
                         q.push(x);
                 }
             }
         }
+        
+         
         return ans;
+        
     }
 };
