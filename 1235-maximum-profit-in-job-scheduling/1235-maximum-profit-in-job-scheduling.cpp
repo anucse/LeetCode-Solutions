35dee3bf43a478dde071ee5ea642e623fd4ceef2
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-06-18 20:41:19 +0530

    Time: 284 ms (34.66%), Space: 55 MB (63.83%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> dp;
+    int recursion(vector<vector<int>> &jobs,int curr_index,int prev_end){
+        
+        if(curr_index==jobs.size())
+            return 0;
+        
+        // exclude current job 
+        if(jobs[curr_index][0]<prev_end){
+            return recursion(jobs,curr_index+1,prev_end);
+        }
+        
+        if(dp[curr_index]!=-1)
+            return dp[curr_index];
+        
+        int res= max(jobs[curr_index][2]+recursion(jobs,curr_index+1,jobs[curr_index][1]),recursion(jobs,curr_index+1,prev_end));
+        
+        return dp[curr_index]=res;
+    }
+    int jobScheduling(vector<int>& start, vector<int>& end, vector<int>& profit) {
+        
+        int n=start.size();
+        vector<vector<int>> jobs(n,vector<int>(3));
+        
+        for(int i=0;i<n;i++){
+            jobs[i][0]=start[i];
+            jobs[i][1]=end[i];
+            jobs[i][2]=profit[i];
+        }
+        
+        sort(jobs.begin(),jobs.end());
+        
+        dp=vector<int>(n,-1);
+        
+        return recursion(jobs,0,0); // jobs,curr_index,prev_end
+    }
+};
