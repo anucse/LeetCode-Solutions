8feba53b8b39db1cbf67a7a5b022ef63ce3b6bb4
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-06-21 23:21:24 +0530

    Time: 3 ms (53.05%), Space: 7.9 MB (24.93%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dp;
+    int solve(vector<int> &nums,int i){
+        if(i>=nums.size())
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+        return dp[i]=max(nums[i]+solve(nums,i+2),solve(nums,i+1));
+    }
+    int rob(vector<int>& nums) {
+        dp.resize(nums.size(),-1);
+        return solve(nums,0);
+    }
+};
