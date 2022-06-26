f5cd222535172dbbd6360e151cd256c1f2a2a67e
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-06-26 21:04:19 +0530

    Time: 97 ms (37.60%), Space: 42.4 MB (80.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -13,19 +13,19 @@ public:
         }
         int sum=0;
         int min_sum=INT_MAX;
-        int i;
-        for(i=0;i<len;i++){
-            sum+=nums[i];
+        int r;
+        for(r=0;r<len;r++){
+            sum+=nums[r];
         }
         
         int l=0;
-        i--;
+        r--;
         
-        while(i<n){
+        while(r<n){
             min_sum=min(sum,min_sum);
-            i++;
-            if(i<n){
-                sum+=nums[i];
+            r++;
+            if(r<n){
+                sum+=nums[r];
             }
             
             sum-=nums[l];
