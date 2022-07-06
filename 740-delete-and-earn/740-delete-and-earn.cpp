97339f3b518682ed615a4ba931b9a3f44abdd946
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-06 15:10:35 +0530

    Time: 11 ms (92.08%), Space: 13.9 MB (48.00%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -6,15 +6,22 @@ public:
             count[x]++;
         }
         
-        vector<int> dp(10001,INT_MIN);
+        //vector<int> dp(10001,INT_MIN);
         
-        dp[0]=count[0];
-        dp[1]=max(count[0],count[1]);
+        int prev2=count[0];
+        int prev1=count[1];
+        
+        int ans;
+        
+        //dp[0]=count[0];
+        //dp[1]=max(count[0],count[1]);
         
         for(int i=2;i<=10000;i++){
-            dp[i]=max(dp[i-1],(count[i]*i)+dp[i-2]);
+            ans=max(prev1,(count[i]*i)+prev2);
+            prev2=prev1;
+            prev1=ans;
         }
         
-        return dp[10000];
+        return ans;
     }
 };
