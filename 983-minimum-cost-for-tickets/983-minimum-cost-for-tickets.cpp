489b2937053acea4a0d2c0ad8c418b5c00882cf9
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-08 13:35:26 +0530

    Time: 7 ms (54.54%), Space: 9.4 MB (84.92%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -28,8 +28,32 @@ public:
         return dp[i]= min({oneDay,sevenDay,thirtyDay});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int> dp(367,-1);
+        vector<int> dp(367,INT_MAX);
         int n=days.size();
-        return solve (days,costs,dp,n,0);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+        
+            // 1 day pass 
+        
+            int oneDay=costs[0]+dp[i+1];
+
+            // 7 days pass
+
+            int j ;
+            for(j=i;j<n && days[j]<days[i]+7;j++);
+
+            int sevenDay=costs[1]+dp[j];
+
+            // 30 days pass 
+
+
+            for(j=i;j<n && days[j]<days[i]+30;j++);
+
+            int thirtyDay=costs[2]+dp[j];
+
+            dp[i]= min({oneDay,sevenDay,thirtyDay}); 
+        }
+        
+        return dp[0];
     }
 };
