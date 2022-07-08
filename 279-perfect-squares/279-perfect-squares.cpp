0f7cc1fbf71ac3cdd46b69a60f205b438b68c840
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-08 12:57:05 +0530

    Time: 314 ms (43.17%), Space: 9.2 MB (49.23%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -15,8 +15,20 @@ public:
         }
         return dp[n]=ans;
     }
+    
+    
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        vector<int> dp(n+1,INT_MAX);
+        
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i]=min(dp[i],dp[i-j*j]+1);
+            }
+        }
+        
+        return dp[n];
     }
 };
