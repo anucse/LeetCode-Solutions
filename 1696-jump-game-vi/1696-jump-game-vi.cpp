c87a688350fe1f67fea7f1ef89f2b3f37c6175ac
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-09 10:30:11 +0530

    Time: 282 ms (59.68%), Space: 90.6 MB (45.19%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        
+        if(n==1)
+            return nums[0];
+        
+        
+        priority_queue<pair<int,int>> pq; // {score,index}
+        
+        pq.push({nums[0],0});
+        
+        int ans;
+        
+        for(int i=1;i<n;i++){
+            while(i>k && (i-k-1)>=pq.top().second )
+                pq.pop();
+            pair<int,int> top=pq.top();
+            int highestScore=top.first;
+            
+            pq.push({highestScore+nums[i],i});
+            if(i==n-1)
+                ans= highestScore+nums[i];
+        }
+        return ans;
+    }
+};
