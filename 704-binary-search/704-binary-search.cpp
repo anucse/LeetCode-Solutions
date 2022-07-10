bb1ae8815039cedfab9f21b830ed557aae8e70fc
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-10 09:20:43 +0530

    Time: 81 ms (10.42%), Space: 27.4 MB (91.98%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,26 +1,29 @@
 class Solution {
+private:
+    int ans=-1;
 public:
-    int ans;
-    int search(vector<int>& nums, int target){
-        int n=nums.size();
-        b_search(nums,target,0,n-1);
-        return ans;
-    }
-    
-    void b_search(vector<int>& nums, int target,int left,int right) {
-        if(left>right){
-            ans=-1;
-            return ;
-        }
-            
+    void solve(vector<int>& nums,int target, int left,int right){
         int mid=(left+right)/2;
-        if(target==nums[mid])
-            ans= mid;
-        else if(target<nums[mid])
-            b_search(nums,target,left,mid-1);
-        else{
-            b_search(nums,target,mid+1,right);
+        
+        if(left>right)
+            return;
+        
+        if(nums[mid]==target){
+            ans=mid;
+            return;
         }
         
+        else if(nums[mid]>target){
+            solve(nums,target, left,mid-1);
+        }
+        
+        else{
+            solve(nums,target, mid+1,right);
+        }
+    }
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        solve(nums,target, 0,n-1);
+        return ans;
     }
 };
