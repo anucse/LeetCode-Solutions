890729f9b5654df6b58da7229df17a664f9db7a6
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-10 09:41:47 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (67.46%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,10 +4,11 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int left=1;
-        int right=n;
+        long left=1;
+        long right=n;
+        
         while(left<right){
-            int mid=left+(right-left)/2;
+            long long mid=(left+right)/2;
             if(isBadVersion(mid)){
                 right=mid;
             }
@@ -15,6 +16,7 @@ public:
                 left=mid+1;
             }
         }
+        
         return left;
     }
 };
