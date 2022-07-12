96afebf6c14d9fa9403c8e1121220e102f35993b
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-12 11:34:54 +0530

    Time: 1667 ms (15.06%), Space: 9.5 MB (67.31%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,33 +1,33 @@
 class Solution {
-public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
-    
-      int sum =accumulate(nums.begin(),nums.end(),0);
-      vector<bool>vis(nums.size(),false);
-      if(sum%k!=0)
-        return false;
-      int s=sum/k;
-      sort(begin(nums),end(nums),greater<int>());// For avoid extra calculation
-      return is_possible(nums,0,s,k,0,vis);
-  }
- bool is_possible(vector<int>&nums,int curr,int sum,int k,int start,vector<bool>&vis)
-{
+private:
+    vector<bool> visited;
+    bool solve(vector<int>& nums,int target,int currSum,int i, int k){
         if(k==1)
             return true;
-        if(start>=nums.size()) //This line is important to avoid tle
-           return false;
-        if(curr==sum)
-                return is_possible(nums,0,sum,k-1,0,vis);
-    
-    for(int i=start;i<nums.size();i++)
-    {
-        if(vis[i] || nums[i]+curr>sum)
-            continue;
-        vis[i]=true;
-        if(is_possible(nums,curr+nums[i],sum,k,i+1,vis))
-            return true;
-        vis[i]=false;
+        if(i>=nums.size())
+            return false;
+        if(currSum==target)
+            return solve(nums,target,0,0,k-1);
+        for(int j=i;j<nums.size();j++){
+            if(visited[j] || target<currSum+nums[j])
+                continue;
+            
+            visited[j]=true;
+            if(solve(nums,target,currSum+nums[j],j+1,k))
+                return true;
+            visited[j]=false;
+        }
+        return false;
     }
-    return false;
-}
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum=0;
+        for(int x:nums)
+            sum+=x;
+        if( sum%k)
+            return false;
+        visited.resize(nums.size(),false);
+        sort(begin(nums),end(nums),greater<int>());
+        return solve(nums,sum/k,0,0,k);
+    }
 };
