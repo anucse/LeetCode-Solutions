e74fd00e4ce7d2aee58d33d2957d28288e637f41
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-13 03:51:25 +0530

    Time: 27 ms (39.72%), Space: 12.8 MB (50.64%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& dict) {
-        vector<bool> dp(s.size()+1,false);
-        dp[0]=true;
-        for(int i=1;i<=s.size();i++){
-            for(int j=0;j<i;j++){
-                if(dp[j] && (find(dict.begin(),dict.end(),s.substr(j,i-j))!=dict.end())){
-                    dp[i]=true;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, false);
+        dp[0] = true;
+        
+        // we mark as true every index that we managed to segment so far
+        for (int i = 1; i <= s.size(); i++)
+            for (int j = 0; j < i; j++)
+                if ((dp[j]) && (find(wordDict.begin(), wordDict.end(), s.substr(j, i-j)) != wordDict.end())) {
+                    dp[i] = true;
                     break;
                 }
-            }
-        }
-        return dp[s.size()];
+        return dp.back();
     }
 };
