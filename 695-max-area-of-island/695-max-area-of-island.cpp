c6c5d4526769bbbf6638c2c2074f90455e68941a
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-16 11:03:27 +0530

    Time: 22 ms (82.07%), Space: 23.2 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,30 +1,31 @@
 class Solution {
-private:
-    int destroyIsland(vector<vector<int>>& grid,int r,int c,int i, int j){
-        if(i<0||j<0||i>=r||j>=c||grid[i][j]!=1)
+public:
+    int destroyIsland(vector<vector<int>>& grid,int r,int c,int i,int j){
+        if(i<0||i>=r||j<0||j>=c||grid[i][j]==0)
             return 0;
+        
         grid[i][j]=0;
         
-        return 1+destroyIsland(grid,r,c,i+1,j)+destroyIsland(grid,r,c,i-1,j)+destroyIsland(grid,r,c,i,j+1)+destroyIsland(grid,r,c,i,j-1);
+        return 1+destroyIsland(grid,r,c,i+1,j)+
+            destroyIsland(grid,r,c,i,j+1)+
+            destroyIsland(grid,r,c,i-1,j)+
+            destroyIsland(grid,r,c,i,j-1);
     }
     
-public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int ans=0;
         int r=grid.size();
         int c=grid[0].size();
         
-        int count;
-        int maxArea=0;
-        
-        for(int i=0;i<r;++i){
-            for(int j=0;j<c;++j){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 if(grid[i][j]==1){
-                    count = destroyIsland(grid,r,c,i,j);
-                    maxArea=max(maxArea,count);
+                    int area=destroyIsland(grid,r,c,i,j);
+                    ans=max(ans,area);
                 }
             }
         }
         
-        return maxArea;
+        return ans;
     }
 };
