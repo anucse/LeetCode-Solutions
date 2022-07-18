fee84a1fcd0dedb23936867209ed0c43a2ebd0fe
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-18 14:58:29 +0530

    Time: 205 ms (14.56%), Space: 30.3 MB (51.34%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-        int r=grid.size();
-        int c=grid[0].size();
-        vector<vector<int>> ans(r,vector<int> (c,INT_MAX));
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int r=matrix.size();
+        int c=matrix[0].size();
         
         queue<pair<int,int>> q;
-        vector<pair<int,int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
+        vector<vector<int>> ans(r,vector<int>(c,INT_MAX));
         
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j]==0){
+                if(matrix[i][j]==0){
                     ans[i][j]=0;
                     q.push({i,j});
                 }
             }
         }
+        
+        vector<pair<int,int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
+        
         while(!q.empty()){
-            pair<int,int> top=q.front();
+            int x=q.front().first;
+            int y=q.front().second;
+            
             q.pop();
-            int x=top.first;
-            int y=top.second;
+            
             for(pair<int,int> d:dir){
                 int nx=x+d.first;
                 int ny=y+d.second;
                 
-                if(nx<0 || nx>=r || ny<0 || ny>=c )
+                if(nx<0 || nx>=r || ny<0 || ny>=c)
                     continue;
+                
                 if(ans[nx][ny]>ans[x][y]+1){
                     ans[nx][ny]=ans[x][y]+1;
                     q.push({nx,ny});
                 }
             }
         }
+        
         return ans;
     }
 };
