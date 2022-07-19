db3a82511fe3b0c78ff1bebf49bc5880010d562c
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-19 14:41:33 +0530

    Time: 21 ms (15.30%), Space: 12.1 MB (24.03%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<string> ans;
-    string temp="";
-    void solve(string s,string temp, int n, int i){
+    void solve(string s,string temp,int n,int i){
         if(i==n){
             ans.push_back(temp);
-            return;
+            return ;
         }
         
         if(isdigit(s[i])){
             temp+=s[i];
             solve(s,temp,n,i+1);
         }
-        else {
+        
+        else{
             string temp1=temp;
             temp1+=tolower(s[i]);
             solve(s,temp1,n,i+1);
             
-            string temp2=temp;
-            temp2+=toupper(s[i]);
-            solve(s,temp2,n,i+1);
+            temp1=temp;
+            temp1+=toupper(s[i]);
+            solve(s,temp1,n,i+1);
         }
     }
     vector<string> letterCasePermutation(string s) {
-        solve(s,"",s.size(),0);
+        int n=s.size();
+        string temp="";
+        solve(s,temp,n,0);
         return ans;
     }
 };
