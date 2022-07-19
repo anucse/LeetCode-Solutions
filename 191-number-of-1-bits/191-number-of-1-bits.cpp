2e861e75206c97496645b3da6a41b268db7eae98
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-19 14:51:05 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (47.68%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,9 +3,9 @@ public:
     int hammingWeight(uint32_t n) {
         int ans=0;
         while(n){
-            if(n&1==1)
+            if(n & 1)
                 ans++;
-            n=(n>>1);
+            n=n>>1;
         }
         return ans;
     }
