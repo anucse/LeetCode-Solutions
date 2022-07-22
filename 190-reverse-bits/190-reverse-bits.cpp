71413ef4e2d942af00ef1fcbaa8303bb34c8753e
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-07-22 09:53:40 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.23%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -3,11 +3,9 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans=0;
         for(int i=0;i<32;i++){
-            int mod=n%2;
-            ans=ans*2+mod;
-            n=n>>1;
+            ans=ans*2+(n%2);
+            n>>=1;
         }
-        
         return ans;
     }
 };
