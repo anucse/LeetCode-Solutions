d20a23da44578ef4d1f97a87f0b5ed38c9a98458
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-16 11:22:19 +0530

    Time: 19 ms (68.79%), Space: 16.8 MB (13.06%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
+    int solve(vector<int> &count,vector<int> &dp,int i){
+        if(i>=10001)
+            return 0; //1-10000
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int take=solve(count,dp,i+2)+count[i]*i;
+        int not_take=solve(count,dp,i+1);
+        
+        return dp[i]= max(take,not_take);
+    }
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> count(10001,0); 
+        int n=nums.size();
+        vector<int> count(10001,0);
+        
         for(int x:nums){
             count[x]++;
         }
         
+        vector<int> dp(10001,-1);
         
-        int prev2=count[0];
-        int prev1=count[1];
+        return solve(count,dp,1);
         
-        int ans;
-        
-        
-        for(int i=2;i<=10000;i++){
-            ans=max(prev1,(count[i]*i)+prev2);
-            prev2=prev1;
-            prev1=ans;
-        }
-        
-        return ans;
     }
 };
