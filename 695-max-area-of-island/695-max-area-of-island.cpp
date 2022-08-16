6e24e9198251ae27662cfe52db21b1d3e6799bb3
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-16 18:06:51 +0530

    Time: 25 ms (69.15%), Space: 23.2 MB (61.52%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int destroyIsland(vector<vector<int>>& grid,int r,int c,int i,int j){
-        if(i<0||i>=r||j<0||j>=c||grid[i][j]==0)
+    int destroyIsland(vector<vector<int>>& grid,int r,int c,int i, int j){
+        if(i<0||j<0||i>=r||j>=c||grid[i][j]==0)
             return 0;
-        
         grid[i][j]=0;
         
         return 1+destroyIsland(grid,r,c,i+1,j)+
-            destroyIsland(grid,r,c,i,j+1)+
             destroyIsland(grid,r,c,i-1,j)+
+            destroyIsland(grid,r,c,i,j+1)+
             destroyIsland(grid,r,c,i,j-1);
+            
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int ans=0;
         int r=grid.size();
         int c=grid[0].size();
         
+        int ans=0;
+        
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j]==1){
-                    int area=destroyIsland(grid,r,c,i,j);
-                    ans=max(ans,area);
-                }
+                if(grid[i][j]==0)
+                    continue;
+                int area=destroyIsland(grid,r,c,i,j);
+                ans=max(ans,area);
             }
         }
         
