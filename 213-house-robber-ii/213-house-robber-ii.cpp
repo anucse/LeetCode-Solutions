f8002cfd469cfe94e325ebb258e4881e479aa500
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-16 10:36:48 +0530

    Time: 3 ms (58.76%), Space: 7.9 MB (42.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int n,int i){
-        vector<int> dp(n,INT_MIN);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+    int solve(vector<int> &nums,int n){
         
-        for(int i=2;i<n;i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        
-        return dp[n-1];
-    }
-    
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> nums1,nums2;
         if(n==1)
             return nums[0];
         if(n==2)
             return max(nums[0],nums[1]);
         
+        int ans;
+        int prev2=nums[0];
+        int prev1=max(nums[0],nums[1]);
         
-        for(int i=0;i<nums.size();i++){
-            if(i!=0)
-                nums1.push_back(nums[i]);
-            if(i!=n-1)
-                nums2.push_back(nums[i]);
+        for(int i=2;i<n;i++){
+            
+            ans=max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=ans;
         }
         
-        return max(solve(nums1,n-1,0),solve(nums2,n-1,0));
+        return ans;
+        
+    }
+    
+    int rob(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        if(n==1)
+            return nums[0];
+        
+        vector<int> nums1,nums2;
+        
+        for(int i=0;i<n;i++){
+            if(i!=0)
+                nums2.push_back(nums[i]);
+            if(i!=n-1)
+                nums1.push_back(nums[i]);
+        }
+        
+        return max(solve(nums1,n-1),solve(nums2,n-1));
     }
 };
