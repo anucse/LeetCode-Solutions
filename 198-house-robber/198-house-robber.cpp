80b24a9a97e837a98553bf58c4fa4154d8ce5611
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-16 10:13:17 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.61%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    
     int rob(vector<int>& nums) {
+        
         int n=nums.size();
+        
         if(n==1)
             return nums[0];
         if(n==2)
@@ -10,11 +11,11 @@ public:
         
         int prev2=nums[0];
         int prev1=max(nums[0],nums[1]);
-        
         int ans;
         
         for(int i=2;i<n;i++){
-            ans=max(prev1,prev2+nums[i]);
+            
+            ans=max(prev2+nums[i],prev1);
             prev2=prev1;
             prev1=ans;
         }
