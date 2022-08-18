ddd1adac0f5bba14207996a8895787bcada10a97
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-18 13:54:02 +0530

    Time: 47 ms (29.05%), Space: 10.8 MB (24.79%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,20 +4,23 @@ public:
         int n=s.size();
         int l=0;
         int r=0;
-        int maxLen=0;
         unordered_set<char> st;
+        int ans=0;
         
         while(r<n){
             if(st.find(s[r])==st.end()){
+                int len=r-l+1;
+                ans=max(ans,len);
                 st.insert(s[r]);
                 r++;
-                maxLen=max(maxLen,(int)st.size());
             }
+            
             else{
                 st.erase(s[l]);
                 l++;
             }
         }
-        return maxLen;
+        
+        return ans;
     }
 };
