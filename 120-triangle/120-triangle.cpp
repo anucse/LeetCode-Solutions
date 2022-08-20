438afd43a37e1eb61f47e72efa1f1639d1a11539
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-20 10:52:50 +0530

    Time: 10 ms (54.76%), Space: 8.7 MB (65.21%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -5,24 +5,26 @@ public:
         int n=matrix.size();
         
         int ans=INT_MAX;
-        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        //vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        vector<int> prev(n,INT_MAX),curr(n,INT_MAX);
         
-        dp[0][0]=matrix[0][0];
+        prev[0]=matrix[0][0];
         
         for(int i=1;i<n;i++){
             for(int j=0;j<=i;j++){
                 if(j==0){
-                    dp[i][j]=dp[i-1][j]+matrix[i][j];
+                    curr[j]=prev[j]+matrix[i][j];
                 }
                 
                 else{
-                    dp[i][j]=min(dp[i-1][j],dp[i-1][j-1])+matrix[i][j];
+                    curr[j]=min(prev[j],prev[j-1])+matrix[i][j];
                 }
             }
+            prev=curr;
         }
         
         for(int j=0;j<n;j++){
-            ans=min(ans,dp[n-1][j]);
+            ans=min(ans,prev[j]);
         }
         
         return ans;
