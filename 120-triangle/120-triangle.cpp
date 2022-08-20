e53f3f8ff06d48d54ed4425e72fb99de018de2fb
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-20 10:49:08 +0530

    Time: 15 ms (21.01%), Space: 9 MB (31.66%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int> (n,INT_MAX));
-        for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle[n-1][j];
-        }
+    int minimumTotal(vector<vector<int>>& matrix) {
         
-        for(int i=n-2;i>=0;i--){
+        int n=matrix.size();
+        
+        int ans=INT_MAX;
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        
+        dp[0][0]=matrix[0][0];
+        
+        for(int i=1;i<n;i++){
             for(int j=0;j<=i;j++){
-                dp[i][j]=min(dp[i+1][j],dp[i+1][j+1])+triangle[i][j];
+                if(j==0){
+                    dp[i][j]=dp[i-1][j]+matrix[i][j];
+                }
+                
+                else{
+                    dp[i][j]=min(dp[i-1][j],dp[i-1][j-1])+matrix[i][j];
+                }
             }
         }
         
-        return dp[0][0];
+        for(int j=0;j<n;j++){
+            ans=min(ans,dp[n-1][j]);
+        }
+        
+        return ans;
     }
 };
