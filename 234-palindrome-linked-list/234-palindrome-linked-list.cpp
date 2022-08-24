647a5e148b13575d26a308692bf2e85e49833ed6
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-24 08:30:58 +0530

    Time: 282 ms (79.89%), Space: 114.2 MB (68.61%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,20 +10,49 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> v;
-        while(head){
-            v.push_back(head->val);
-            head=head->next;
+    ListNode* findMid(ListNode* head){
+        if(!head || !head->next)
+          return head;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
         }
-        int i=0,j=v.size()-1;
-        while(i<j){
-            if(v[i]!=v[j])
+        return slow;
+    }
+    
+    ListNode* reverse(ListNode* head){
+        if(!head || !head->next)
+            return head;
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* temp=head->next;
+        while(curr){
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+            if(temp)
+                temp=temp->next;
+        }
+        return prev;
+    }
+    
+    bool compare(ListNode* head1,ListNode* head2){
+        while(head1 && head2){
+            if(head1->val != head2->val)
                 return false;
-            i++;
-            j--;
+            head1=head1->next;
+            head2=head2->next;
         }
-        
         return true;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        if(!head ||!head->next)
+            return true;
+        ListNode* mid=findMid(head);
+        ListNode* head2=reverse(mid);
+        return compare(head,head2);
     }
 };
