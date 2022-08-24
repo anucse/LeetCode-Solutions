3301d317848829ccecf16c2ac0d754d63a102228
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-24 09:30:49 +0530

    Time: 11 ms (81.22%), Space: 9.7 MB (80.90%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -4,20 +4,23 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
-        vector<vector<int>> dp(r,vector<int>(c,INT_MAX));
-        dp[r-1][c-1]=grid[r-1][c-1];
+        
+        vector<int> prev(c,0);
+        vector<int> curr(c,0);
+        prev[c-1]=grid[r-1][c-1];
         for(int j=c-2;j>=0;j--){
-            dp[r-1][j]=dp[r-1][j+1]+grid[r-1][j];
-        }
-        for(int i=r-2;i>=0;i--){
-            dp[i][c-1]=dp[i+1][c-1]+grid[i][c-1];
+            prev[j]=prev[j+1]+grid[r-1][j];
         }
         
         for(int i=r-2;i>=0;i--){
-            for(int j=c-2;j>=0;j--){
-                dp[i][j]=min(dp[i+1][j],dp[i][j+1])+grid[i][j];
+            for(int j=c-1;j>=0;j--){
+                if(j==c-1)
+                    curr[j]=prev[j]+grid[i][j];
+                else
+                    curr[j]=min(curr[j+1],prev[j])+grid[i][j];
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
