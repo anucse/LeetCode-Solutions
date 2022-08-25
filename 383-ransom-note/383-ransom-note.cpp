19723299a2a1f2117535535cb6f2ee704fe1147c
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-25 14:00:34 +0530

    Time: 18 ms (74.92%), Space: 8.9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> m;
-        for(char c:magazine)
+        for(char c:magazine){
             m[c]++;
+        }
         
         for(char c:ransomNote){
-            if(m.find(c)==m.end()){
+            if(m.find(c)==m.end())
                 return false;
-            }
-            else{
-                if(m[c]==1)
-                    m.erase(c);
-                else
-                    m[c]--;
-            }
+            m[c]--;
+            if(m[c]==0)
+                m.erase(c);
         }
+        
         return true;
     }
 };
