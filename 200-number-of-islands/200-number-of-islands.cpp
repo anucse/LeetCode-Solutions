ffe524d8f64b85fcc391551b02c9aadc834df8c2
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-08-29 08:09:21 +0530

    Time: 41 ms (83.82%), Space: 12.3 MB (83.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void destroyIsland(vector<vector<char>>& grid,int i,int j,int r,int c){
+    void destroyIsland(vector<vector<char>>& grid,int r,int c,int i,int j){
         if(i<0||j<0||i>=r||j>=c||grid[i][j]=='0')
             return ;
-        grid[i][j]='0';
-        destroyIsland(grid,i+1,j,r,c);
-        destroyIsland(grid,i,j+1,r,c);
-        destroyIsland(grid,i-1,j,r,c);
-        destroyIsland(grid,i,j-1,r,c);
         
+        grid[i][j]='0';
+        destroyIsland(grid,r,c,i+1,j);
+        destroyIsland(grid,r,c,i-1,j);
+        destroyIsland(grid,r,c,i,j+1);
+        destroyIsland(grid,r,c,i,j-1);
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
         int ans=0;
-        
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(grid[i][j]=='1'){
-                    ans++;;
-                    destroyIsland(grid,i,j,r,c);
+                    destroyIsland(grid,r,c,i,j);
+                    ans++;
                 }
             }
         }
