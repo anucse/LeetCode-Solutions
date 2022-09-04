e9ba5d128f9187fdda9bf58874468dea44c193d7
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-09-04 09:27:23 +0530

    Time: 12 ms (30.21%), Space: 12 MB (88.48%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -19,8 +19,7 @@ public:
             int n=q.size();
             multiset<pair<int,int>> temp;
             for(int i=0;i<n;i++){
-                auto top=q.front();
-                q.pop();
+                auto top=q.front(); q.pop();
                 temp.insert({top.second->val,top.first});
                 if(top.second->left)
                     q.push({top.first-1,top.second->left});
@@ -30,10 +29,10 @@ public:
             for(auto x:temp)
                 m[x.second].push_back(x.first);
         }
-        
         vector<vector<int>> ans;
-        for(auto x:m)
+        for(auto x:m){
             ans.push_back(x.second);
+        }
         
         return ans;
     }
