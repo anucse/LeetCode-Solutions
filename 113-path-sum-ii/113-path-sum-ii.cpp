c31460a6d33de99b8b2cf8ee7dad676f51f565dd
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-09-24 08:42:00 +0530

    Time: 11 ms (86.80%), Space: 20 MB (60.91%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -12,35 +12,40 @@
 class Solution {
 public:
     vector<vector<int>> ans;
+    
+    
     void dfs(TreeNode* root, int target,vector<int>& temp, int sum){
         if(!root)
-            return ;
+            return;
         if(!root->left && !root->right){
-            sum+=root->val;
-            temp.push_back(root->val);
             if(sum==target){
-                sum-=root->val;
                 ans.push_back(temp);
-                temp.pop_back();
-            }
-            else{
-                sum-=root->val;
-                
                 temp.pop_back();
                 return;
             }
+            
         }
         
-        temp.push_back(root->val);
+        if(root->left){
+            temp.push_back(root->left->val);
+            dfs(root->left,target,temp,sum+root->left->val);
+        }
+        if(root->right){
+            temp.push_back(root->right->val);
+            dfs(root->right,target,temp,sum+root->right->val);
+        }
         
-        dfs(root->left,target,temp,sum+root->val);
-        dfs(root->right,target,temp,sum+root->val);
         temp.pop_back();
-        sum-=root->val;
+            
     }
+    
+    
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if(!root)
+            return {};
         vector<int> temp;
-        dfs(root,targetSum,temp,0);
+        temp.push_back(root->val);
+        dfs(root,targetSum,temp,root->val);
         return ans;
     }
 };
