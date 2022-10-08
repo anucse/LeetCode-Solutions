98c19bdc7f5b8e85d245b553ae3c02035c828dba
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2022-10-08 10:42:11 +0530

    Time: 267 ms (90.25%), Space: 16.5 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -4,12 +4,15 @@ public:
         
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        int diff=INT_MAX;
-        int sum=0;
+        
         int ans;
+        int diff=INT_MAX;
+        int sum;
+        
         for(int i=0;i<n;i++){
             int j=i+1;
             int k=n-1;
+            
             while(j<k){
                 sum=nums[i]+nums[j]+nums[k];
                 if(sum==target)
@@ -19,8 +22,8 @@ public:
                 else
                     j++;
                 if(diff>abs(sum-target)){
-                    diff=abs(sum-target);
                     ans=sum;
+                    diff=abs(sum-target);
                 }
             }
         }
