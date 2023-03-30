e027c24d8582154d40b0eecb38b87fd7b4b73d5a
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-03-30 15:10:46 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (28.88%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
-   
+    bool solve(string s, string t,int i,int j, int m, int n){
+        
+        if(i==m)
+            return 1;
+        if(j==n)
+            return 0;
+        
+        
+        if(s[i]==t[j])
+            return solve(s,t,i+1,j+1,m,n);
+        else 
+            return solve(s,t,i,j+1,m,n);
+    }
+    
+    
     bool isSubsequence(string s, string t) {
         
         int m=s.size();
