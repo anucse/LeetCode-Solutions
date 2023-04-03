736ac7d93bfd70c79edff925df9a6015dfd8f262
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-03 13:32:39 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (36.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,15 +5,22 @@ public:
         
         int n=nums.size();
         vector<int> dp(n,INT_MIN);
+        if(n==1)
+            return nums[0];
         
-        dp[0]=nums[0];
+        int prev2=nums[0];
+        int prev1;
         if(n>1)
-            dp[1]=max(nums[0],nums[1]);
+            prev1=max(nums[0],nums[1]);
+        
+        int ans=prev1;
         
         for(int i=2;i<n;i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            ans=max(nums[i]+prev2,prev1);
+            prev2=prev1;
+            prev1=ans;
         }
         
-        return dp[n-1];
+        return ans;
     }
 };
