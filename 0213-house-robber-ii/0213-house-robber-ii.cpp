ecd2fc05fadb5de59f8715a238eb3fd776727d09
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-03 14:56:22 +0530

    Time: 2 ms (50.42%), Space: 7.9 MB (44.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -24,11 +24,9 @@ public:
     int rob(vector<int>& nums) {
         
         int n=nums.size();
+        
         if(n==1)
             return nums[0];
-        if(n==2)
-            return max(nums[0],nums[1]);
-        
         
         vector<int> nums1(nums.begin(),nums.end()-1);
         vector<int> nums2(nums.begin()+1,nums.end());
