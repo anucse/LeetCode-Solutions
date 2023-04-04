a8fada79a11acfb77f4a736b96a0f7442e8084b7
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-04 15:22:02 +0530

    Time: 86 ms (77.17%), Space: 18.6 MB (42.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,27 +3,32 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         
-        vector<vector<int>> dp(n,vector<int> (amount+1,1e9));
+        //vector<vector<int>> dp(n,vector<int> (amount+1,1e9));
+        //dp[i-1]->prev
+        //dp[i] -> curr
+        vector<int> prev(amount+1,1e9);
+        vector<int> curr(amount+1,1e9);
         
         for(int i=0;i<n;i++){
             for(int j=0;j<=amount;j++){
                 if(i==0){
                     if(j%coins[0]==0)
-                        dp[i][j]=j/coins[0];
+                        curr[j]=j/coins[0];
                     else
-                        dp[i][j]=1e9;
+                        curr[j]=1e9;
                 }
                 else{
                     int t=1e9;
                     if(j>=coins[i])
-                        t=dp[i][j-coins[i]]+1;
-                    int nt=dp[i-1][j];
-                    dp[i][j]=min(t,nt);
+                        t=curr[j-coins[i]]+1;
+                    int nt=prev[j];
+                    curr[j]=min(t,nt);
                 }
             }
+            prev=curr;
         }
         
-        int ans=dp[n-1][amount];
+        int ans=curr[amount];
         return ans==1e9?-1:ans;
     }
 };
