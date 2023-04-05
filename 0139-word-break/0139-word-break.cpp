f3d55bca4271794fc3a093ad969ad6a038d91db6
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-05 17:57:28 +0530

    Time: 16 ms (48.38%), Space: 13.3 MB (33.18%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    vector<int> dp;
-    bool solve(string s,unordered_set<string> &st,int i){
-        
-        if(i==s.size())
-            return true;
-        
-        if(dp[i]!=-1)
-            return dp[i];
-        
-        for(int j=i;j<s.size();j++){
-            if(st.count(s.substr(i,j-i+1)) && solve(s,st,j+1)){
-                return dp[i]=1;
-            }
-        }
-        
-        return dp[i]=0;
-        
-    }
+    vector<bool> dp;
+    
     bool wordBreak(string s, vector<string>& d) {
         unordered_set<string> st(d.begin(),d.end());
         int n=s.size();
-        dp.resize(n+1,-1);
-        return solve(s,st,0);
+        dp.resize(n+1,false);
+        
+        dp[n]=true;
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(st.count(s.substr(i,j-i+1)) && dp[j+1]){
+                    dp[i]=true;
+                    break;
+                }
+            }
+            
+            
+        }
+        
+        return dp[0];
     }
 };
