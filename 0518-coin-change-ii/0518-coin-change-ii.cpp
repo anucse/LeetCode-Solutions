42b40691bab33250feb30de2f19504bc16e896b0
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-05 19:45:12 +0530

    Time: 57 ms (22.11%), Space: 18.6 MB (43.66%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int>& coins,int target, vector<vector<int>> &dp,int n,int i){
-        
-        if(i>=n || target<0)
-            return 0;
-        if(target==0)
-            return 1;
-        if(dp[i][target]!=-1)
-            return dp[i][target];
-        int t=0;
-        if(target>=coins[i])
-            t=solve(coins,target-coins[i],dp,n,i);
-        int nt=solve(coins,target,dp,n,i+1);
-        
-        return dp[i][target]=t+nt;
-    }
+    
     int change(int target, vector<int>& coins) {
         
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int> (target+1,-1));
-        return solve(coins,target,dp,n,0);
+        vector<vector<int>> dp(n+1,vector<int> (target+1,0));
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=target;j++){
+                if(j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int t=0;
+                    if(j>=coins[i])
+                        t=dp[i][j-coins[i]];
+                    int nt=dp[i+1][j];
+
+                    dp[i][j]=t+nt;
+                }
+            }
+        }
+        return dp[0][target];
+        
     }
 };
