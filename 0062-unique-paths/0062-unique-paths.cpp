f1166c8478e388a20c6f86f8a55e21a64b408cf9
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-05 18:11:24 +0530

    Time: 5 ms (14.39%), Space: 6.5 MB (55.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -19,7 +19,27 @@ public:
     }
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return solve(m,n,dp,0,0);
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        
+        
+        for(int i=0;i<m;i++){
+            dp[i][n-1]=1;
+        }
+        
+        for(int j=0;j<n;j++){
+            dp[m-1][j]=1;
+        }
+        
+        
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                int right=dp[i][j+1];
+                int down= dp[i+1][j];
+
+                dp[i][j]=right+down;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
