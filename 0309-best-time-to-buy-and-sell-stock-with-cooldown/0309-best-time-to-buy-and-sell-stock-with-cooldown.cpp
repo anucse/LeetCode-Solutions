418542e3d965fe1e1f5ab03ad4b0067a98739612
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-06 13:11:55 +0530

    Time: 4 ms (71.88%), Space: 11.8 MB (56.71%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -17,8 +17,22 @@ public:
         
     }
     int maxProfit(vector<int>& prices) {
+        
         int n=prices.size();
-        dp.resize(n+1,vector<int> (2,-1));
-        return solve(prices,n,0,0);
+        dp.resize(n+2,vector<int> (2,0));
+        // return solve(prices,n,0,0);
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(!j){
+                    dp[i][j]=max(dp[i+1][1]-prices[i],dp[i+1][0]);
+                }
+                else{
+                    dp[i][j]=max(dp[i+2][0]+prices[i],dp[i+1][1]);
+                }
+            }
+        }
+        
+        return dp[0][0];
+        
     }
 };
