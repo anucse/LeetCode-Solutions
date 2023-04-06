62b09391eb18f22d52f0bf9f277c7b6452fb1c08
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-06 11:31:54 +0530

    Time: 15 ms (38.36%), Space: 25.6 MB (25.62%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -5,14 +5,14 @@ public:
         bool match=false;
         if(k==n3)
             return true;
-        if(dp[i][j]!=0)
+        if(dp[i][j]!=-1)
             return dp[i][j]==1;
         if(i<n1 && s1[i]==s3[k])
             match= match || solve(dp,s1,s2,s3,n1,n2,n3,i+1,j,k+1); 
         if(j<n2 && s2[j]==s3[k])
             match= match || solve(dp,s1,s2,s3,n1,n2,n3,i,j+1,k+1);
         
-        dp[i][j]=match?1:-1;
+        dp[i][j]=match?1:0;
         
         return match;
         
@@ -25,7 +25,7 @@ public:
         
         if(n1+n2 != n3 )
             return false;
-        vector<vector<int>> dp(n1+1,vector<int> (n2+1,0));
+        vector<vector<int>> dp(n1+1,vector<int> (n2+1,-1));
         
         return solve(dp,s1,s2,s3,n1,n2,n3,0,0,0);
         
