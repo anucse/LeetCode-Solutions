951fa694f310d75fe7eb4f47713cd8c65b771fe8
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2023-04-07 19:33:26 +0530

    Time: 27 ms (56.20%), Space: 29 MB (21.32%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -18,20 +18,7 @@ public:
         if(!p || !q )
             return false;
         
-        stack<pair<TreeNode*, TreeNode*>> st;
-        st.push({p,q});
-        
-        while(!st.empty()){
-            auto [node1,node2]=st.top();
-            st.pop();
-            if(!node1 && !node2) continue;
-            if(!node1 || !node2) return false;
-            if(node1->val != node2->val) return false;
-            st.push({node1->left,node2->left});
-            st.push({node1->right,node2->right});
-        }
-        
-        return true;
+        return (p->val == q->val ) && sameTree(p->left,q->left) && sameTree(p->right,q->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if(!root && !subRoot)
