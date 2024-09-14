1f7bcb2c2bcfe52498b905aef55790d0566ea4c8
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2024-09-14 16:08:05 +0530

    Time: 0 ms (100%), Space: 12.1 MB (77.76%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,25 +12,12 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
         if(!p && !q)
             return true;
-        if(!p || !q )
+        else if(!p || !q)
             return false;
-        
-        stack<pair<TreeNode*, TreeNode*>> st;
-        st.push({p,q});
-        
-        while(!st.empty()){
-            auto [node1,node2]=st.top();
-            st.pop();
-            if(!node1 && !node2) continue;
-            if(!node1 || !node2) return false;
-            if(node1->val != node2->val) return false;
-            st.push({node1->left,node2->left});
-            st.push({node1->right,node2->right});
-        }
-        
-        return true;
+        else if(p->val == q->val)
+            return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+        return false;
     }
 };
