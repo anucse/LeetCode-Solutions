0ad80021f2bcdae4c751f246f6b8ea5d98b74bd3
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2024-09-18 00:44:08 +0530

    Time: 64 ms (41.06%), Space: 61.7 MB (26.81%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,32 +2,18 @@ class Solution {
 public:
     int maxArea(vector<int>& nums) {
         int n=nums.size();
-        
-        int i=0;
-        int j=n-1;
-        int ans=0;
-        
-        while(i<=j){
-            int area=min(nums[i],nums[j])*(j-i);
-            ans=max(ans,area);
-            
-            int icurr=nums[i];
-            int jcurr=nums[j];
-            
-            if(nums[i]<nums[j]){
-                while(i<n && nums[i]<=icurr){
-                    i++;
-                }
-            }
-            
-            else {
-                while(j>=0 && nums[j]<=jcurr){
-                    j--;
-                }
-            }
+        int l=0;
+        int r=n-1;
+        int ans=INT_MIN;
+        while(l<=r){
+            ans=max(ans,min(nums[l],nums[r])*(r-l));
+            if(nums[l]<nums[r])
+                l++;
+            else
+                r--;
         }
-        
+
         return ans;
-        
+
     }
 };
