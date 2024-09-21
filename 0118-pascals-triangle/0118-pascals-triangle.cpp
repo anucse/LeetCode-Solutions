3be9aaf40b69f2142f5c31d24ff628167f3a2491
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2024-09-22 02:02:34 +0530

    Time: 4 ms (33.9%), Space: 8.2 MB (42.04%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n) {
-        
-        vector<vector<int>> ans(n);
-        
-        for(int i=0;i<n;i++){
+    vector<vector<int>> generate(int r) {
+        vector<vector<int>> ans(r);
+        for(int i=0;i<r;i++){
             ans[i].resize(i+1,1);
             for(int j=1;j<i;j++){
                 ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
             }
         }
-        
+
         return ans;
     }
 };
