dde9486b0d0193c342bea85c825cad2c6dd1c9ac
ANULEKHA DATTA <87842739+anucse@users.noreply.github.com>
2026-02-19 00:10:28 +0100

    Time: 91 ms (31.1%), Space: 26 MB (32.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.size();
-        string ans="";
-        int sum=0;
-        for(int i=0;i<n;i++){
-            int l=i;
-            int r=i;
-            while(l>=0 && r<=n && s[l]==s[r]){
-                if(r-l+1>sum){
-                    sum=r-l+1;
-                    ans=s.substr(l,sum);
-                    
-                }
-                l--;
-                r++;
-            }
-            l=i;
-            r=i+1;
-            while(l>=0 && r<=n && s[l]==s[r]){
-                if(r-l+1>sum){
-                    sum=r-l+1;
-                    ans=s.substr(l,sum);
-                    
-                }
-                l--;
-                r++;
-            }
+        int len=s.length();
 
-            
+        if(len<=1)
+            return s;
+        vector<vector<bool>> dp(len,vector<bool>(len,false));
 
+        int ans=1;
+        int start=0;
+        int end=0;
+
+        for(int i=0;i<len;i++){
+            for (int j=0;j<len;j++){
+                if(i==j) dp[i][j]=true;
+            }
         }
 
-        return ans;
+        for(int i=0;i<len;i++){
+            for (int j=0;j<i;j++){
+                //if(i==j) dp[i][j]=true;
+
+                if(s[i]==s[j] && (dp[j+1][i-1]||i-j<=2)) {
+                    dp[j][i]=true;
+                    if(i-j+1>ans){
+                        ans= i-j+1;
+                        start=j;
+                        end=i;
+                    }
+                }
+            }
+        }
+
+        return s.substr(start,end-start+1);
+
     }
 };
